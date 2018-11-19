b979a0c9981a47adc9eac375b0f3a1c9d9f452a1
jdm79 <james.malvern@gmail.com>
2018-11-19 18:13:13 +0000

    imports body-parser library and adds body-parser middleware to deal with requests before they hit routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const routes = require('./routes/routes')
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json())
 routes(app);
 
 module.exports = app;
