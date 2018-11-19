5c21c261e458bd81a2964ef86c14d97169e38195
jdm79 <james.malvern@gmail.com>
2018-11-19 17:18:39 +0000

    creates a controllers folder and places_controller which extracts the response logic from the route

--- test/app_test.js
+++ test/app_test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../app');
 
+// defines and tests a single route handler
 describe('the express app', () => {
   it('handles a GET request to /api', done => {
     request(app)
