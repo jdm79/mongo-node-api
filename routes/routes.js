90dc082d48ac25ec3191c17e04a9e47539fd2b66
jdm79 <james.malvern@gmail.com>
2018-11-19 18:25:32 +0000

    adds a post route using the create method

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,8 @@
 const PlacesController = require('../controllers/places_controller');
 
 module.exports = app => {
+  // basic response for now
   app.get('/api', PlacesController.greeting);
+  // creates new place
+  app.post('/api/places', PlacesController.create);
 }
