892a0b96ccdcff60218ed46d9eaaedf1c3f13d53
jdm79 <james.malvern@gmail.com>
2018-11-20 21:06:56 +0000

    adds a conditional to check if NODE_ENV is test, otherwise use dev/prod db

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+if(process.env.NODE_ENV !=== 'test') {
+  mongoose.connect('mongodb://localhost/gf-places');
+}
+
 app.use(bodyParser.json())
 routes(app);
 
