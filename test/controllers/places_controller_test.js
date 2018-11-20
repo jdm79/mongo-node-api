f5b1dab89b79d5e53ce886ffb8f621b9b7c053e3
jdm79 <james.malvern@gmail.com>
2018-11-20 22:09:16 +0000

    adds dotenv setup and requires in secret info to use in the mlab address for test db

--- test/controllers/places_controller_test.js
+++ test/controllers/places_controller_test.js
@@ -1,16 +1,25 @@
 const assert = require('assert');
 const request = require('supertest');
+const mongoose = require('mongoose');
 const app = require('../../app');
+
+const Place = mongoose.model('place');
 
 // defines and tests a single route handler
 describe('Places controller', () => {
   // method/route/result
   it('POST to /api/places creates a new place', done => {
-    request(app)
+    Place.count().then(count => {
+      request(app)
       .post('api/places')
       .send({ name: 'Vida Bakery' })
       .end(() => {
+        Driver.count().then(newCount => {
+          assert(count+1 === newCount);
+          done();
+        })
         done();
       })
+    })
   });
 });
