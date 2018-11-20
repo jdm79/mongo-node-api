65342a6ed3c83666e6eeb80e9cc78fb0a8815cc5
jdm79 <james.malvern@gmail.com>
2018-11-20 22:08:51 +0000

    adds dotenv setup and requires in secret info to use in the mlab address

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const routes = require('./routes/routes')
+const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DB_PW = process.env.DB_PASSWORD;
+const DB_USER = process.env.DB_USERNAME;
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-if(process.env.NODE_ENV !=== 'test') {
-  mongoose.connect('mongodb://localhost/gf-places');
+if(process.env.NODE_ENV !== 'test') {
+  mongoose.connect(`mongodb://${DB_USER}:${DB_PW}@ds125392.mlab.com:25392/gf_places`);
 }
 
 app.use(bodyParser.json())
